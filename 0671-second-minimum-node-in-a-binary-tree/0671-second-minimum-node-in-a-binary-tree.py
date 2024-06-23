3136ff05cd74bf65bf55b4b01c68d16bd4ce604e
Nandani <52810541+Nandini2510@users.noreply.github.com>
2024-06-23 11:52:34 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        ans = set()
+        if root is None:
+            return -1
+        def dfs(root):
+            if root is None:
+                return
+            nonlocal ans
+            dfs(root.left)
+            ans.add(root.val)
+            dfs(root.right)
+
+        dfs(root)
+        ans = list(ans)
+        if len(ans) == 1:
+            return -1
+        else:
+            return ans[1]
+        
