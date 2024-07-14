ea7fff64ca49417e724cfab6f573a659ef29bc66
Nandani <52810541+Nandini2510@users.noreply.github.com>
2024-07-14 12:38:42 -0400

    Time: 89 ms (94.75%), Space: 18.4 MB (81.78%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,13 +4,14 @@ class Solution:
         maxL = height[i]
         maxR = height[j]
         res = 0
+
         while i < j:
             if maxL <= maxR:
-                i = i+1
+                i += 1
                 maxL = max(maxL, height[i])
                 res += maxL - height[i]
             else:
-                j = j - 1
+                j -= 1
                 maxR = max(maxR, height[j])
                 res += maxR - height[j]
         return res
