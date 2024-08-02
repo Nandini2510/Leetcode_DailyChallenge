f6a384165598f9880f70ca8ff40094e0654a6538
Nandani <52810541+Nandini2510@users.noreply.github.com>
2024-08-02 10:31:32 -0400

    Time: 197 ms (22%), Space: 18.2 MB (27.5%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,14 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        l, r = 0, len(nums) - 1
 
-        while left <= right:
-            mid = (left + right) // 2
+        while l <= r:
+            mid = l + (r - l) // 2
             if nums[mid] == target:
                 return mid
-            if nums[mid] > target:
-                right = mid - 1
+            elif nums[mid] > target:
+                r = mid - 1
             else:
-                left = mid + 1
+                l = mid + 1
         return -1
 
+        
