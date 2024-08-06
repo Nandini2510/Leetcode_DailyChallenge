17371aa5aa549dd7731b4ce69bfd53697ef16d9b
Nandani <52810541+Nandini2510@users.noreply.github.com>
2024-08-06 11:29:53 -0400

    Time: 41 ms (37.09%), Space: 17.3 MB (64.51%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -6,28 +6,18 @@ class Node:
         self.next = next
         self.random = random
 """
-"""
-Algorithm
-1. Recursive approach starting from head node
-2. Make a dictionary - to keep track of cloned nodes
-3. If we have a cloned copy in dict, we can use that reference
-4. IF we don't have a cloned copy in dict, we add a new node and add it to the vis dict
-5. We make two recursive calls, one with random and other with next pointer
-"""
+
 class Solution:
-    def __init__(self):
-         self.mp = {}
-
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if head == None:
-            return None
-        
-        if head in self.mp:
-            return self.mp[head]
-        node = Node(head.val, None, None)
-        self.mp[head] = node
-
-        node.next = self.copyRandomList(head.next)
-        node.random = self.copyRandomList(head.random)
-
-        return node
+        mp = {None: None}
+        curr = head
+        while curr:
+            mp[curr] = Node(curr.val)
+            curr = curr.next
+        curr = head
+        while curr:
+            copy = mp[curr]
+            copy.next = mp[curr.next]
+            copy.random = mp[curr.random]
+            curr = curr.next
+        return mp[head]
