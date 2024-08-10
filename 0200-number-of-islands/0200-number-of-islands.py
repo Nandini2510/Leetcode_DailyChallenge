ddaa0fd8a42a2b3638e067ac75e9f45ccb213b89
Nandani <52810541+Nandini2510@users.noreply.github.com>
2024-08-10 09:57:13 -0400

    Time: 237 ms (68.88%), Space: 19 MB (57.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
-        res = 0
+        count = 0
 
-        def dfs(i, j, m, n):
-            if i < 0 or i >= m or j < 0 or j >=n or grid[i][j] != "1":
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] == "0" or grid[i][j] == "X":
                 return
-            grid[i][j] = "*"
-
-            dfs(i+1, j, m, n)
-            dfs(i-1, j, m, n)
-            dfs(i, j + 1, m, n)
-            dfs(i, j - 1, m, n)
-
-
+            grid[i][j] = "X"
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1":
-                    res += 1
-                    dfs(i,j,m,n)
-        return res
-
+                    dfs(i, j)
+                    count += 1
+        return count
