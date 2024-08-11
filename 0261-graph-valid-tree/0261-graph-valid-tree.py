583624aa9906eb43baa278e5d7fe2c238be324c6
Nandani <52810541+Nandini2510@users.noreply.github.com>
2024-08-11 13:30:08 -0400

    Time: 89 ms (42.63%), Space: 18.3 MB (74.52%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,21 +1,37 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.rank = [0] * n
+        self.parent = list(range(n))
+        self.size = [1] * n
+    
+    def findUPar(self, node):
+        if node == self.parent[node]:
+            return node
+        self.parent[node] = self.findUPar(self.parent[node])
+        return self.parent[node]
+    
+    def unionByRank(self, u, v):
+        ulp_u = self.findUPar(u)
+        ulp_v = self.findUPar(v)
+        if ulp_u == ulp_v:
+            return False
+        if self.rank[ulp_u] < self.rank[ulp_v]:
+            self.parent[ulp_u] = ulp_v
+        elif self.rank[ulp_u] > self.rank[ulp_v]:
+            self.parent[ulp_v] = ulp_u
+        else:
+            self.parent[ulp_v] = ulp_u
+            self.rank[ulp_u] += 1
+        return True
+
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        adjList = defaultdict(list)
-        vis = set()
-        for edge in edges:
-            adjList[edge[0]].append(edge[1])
-            adjList[edge[1]].append(edge[0])
+        if len(edges) != n - 1:
+            return False
         
-        def dfs(edge, prev):
-            if edge in vis:
+        ds = DisjointSet(n)
+
+        for u, v in edges:
+            if not ds.unionByRank(u, v):
                 return False
-            vis.add(edge)
-            for nei in adjList[edge]:
-                if nei == prev:
-                    continue
-                if not dfs(nei, edge):
-                    return False
-        
-            return True
-        
-        return dfs(0, -1) and len(vis) == n
+        return True
