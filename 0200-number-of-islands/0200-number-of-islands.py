5e367b6e7c13e25d843ffe2a148efa805d2b185e
Nandani <52810541+Nandini2510@users.noreply.github.com>
2024-08-15 18:57:05 -0400

    Time: 242 ms (61.09%), Space: 18.9 MB (77.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m, n = len(grid), len(grid[0])
         res = 0
-        def dfs(i, j):
-            if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] == "0" or grid[i][j] == "X":
-                return
-            grid[i][j] = "X"
+        m, n = len(grid), len(grid[0])
 
-            # bottom
-            dfs(i+1, j)
-            # top
-            dfs(i - 1, j)
-            #right
-            dfs(i, j + 1)
-            #left
-            dfs(i, j - 1)
+        def bfs(i, j):
+            q = deque()
+            q.append((i,j))
+            grid[i][j] = "0"
+
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0], [-1, 0], [0,1], [0,-1]]
+
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if 0 <= r < m and 0 <= c < n and grid[r][c] == "1":
+                        q.append((r,c))
+                        grid[r][c] = "0"
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1":
-                    dfs(i, j)
                     res += 1
+                    bfs(i, j)
         return res
