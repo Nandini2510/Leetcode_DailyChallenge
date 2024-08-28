993af94377846c16f95f42b1685a85705160505e
Nandani <52810541+Nandini2510@users.noreply.github.com>
2024-08-28 14:45:06 -0400

    Time: 458 ms (72.4%), Space: 45 MB (27.32%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,22 +1,23 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+
+        def dfs(nums, n, subset_sum):
+            if subset_sum == 0:
+                return True
+            if n == 0 or subset_sum < 0:
+                return False
+            if memo[n][subset_sum] != -1:
+                return memo[n][subset_sum]
+            res = (dfs(nums, n - 1, subset_sum - nums[n-1]) or dfs(nums, n - 1, subset_sum ))
+            memo[n][subset_sum] = res
+            return res
+        
         total_sum = sum(nums)
+
         if total_sum % 2 != 0:
             return False
-        target = total_sum // 2
-        cache = {}
-
-        def dfs(i, target):
-            if (i, target) in cache:
-                return cache[(i, target)]
-            if i >= len(nums) or target < 0:
-                return False
-            if target == 0:
-                return True
-            if i + 1 < len(nums) and (dfs(i+1, target - nums[i]) or 
-            (dfs(i+1, target))):
-                return True
-            cache[(i, target)] = False
-            return cache[(i, target)]
         
-        return dfs(0, target)
+        subset_sum = total_sum // 2
+        n = len(nums)
+        memo = [[-1 for _ in range(subset_sum + 1)] for _ in range(n + 1)]
+        return dfs(nums, n - 1, subset_sum)
