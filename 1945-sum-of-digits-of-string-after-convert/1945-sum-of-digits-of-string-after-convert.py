8713ff36ad41a70005b4c5075162b360148d243c
Nandani <52810541+Nandini2510@users.noreply.github.com>
2024-09-04 16:57:28 -0400

    Time: 43 ms (21.48%), Space: 16.5 MB (74.92%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
@@ -1,17 +1,18 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
-        # Convert each character to its numerical value and build a string
-        numeric_string = ""
+        mp = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5, 'f' : 6, 'g': 7, 'h' : 8,'i' : 9,'j' : 10, 'k' : 11, 'l' : 12, 'm' : 13,'n' : 14,'o' : 15,'p' : 16,'q' : 17,'r' : 18,
+'s' : 19,'t' : 20,'u' : 21,'v' : 22,'w' : 23,'x' : 24,'y' : 25, 'z' : 26 
+        }
+
+        st = ""
         for ch in s:
-            numeric_string += str(ord(ch) - ord("a") + 1)
-
-        # Apply digit sum transformations k times
+            st += str(mp[ch])
+        
+        num = list(st)
+        res = 0
         while k > 0:
-            digit_sum = 0
-            for digit in numeric_string:
-                digit_sum += int(digit)
-            numeric_string = str(digit_sum)
+            res = sum(int(digit) for digit in num)
+            num = str(res)
             k -= 1
+        return int(num)
 
-        # Convert the final string to integer and return
-        return int(numeric_string)
