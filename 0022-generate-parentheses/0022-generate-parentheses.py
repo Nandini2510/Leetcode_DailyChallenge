673a9eec7277d8da3178f83dfe91b1cdddea4a5e
Nandani <52810541+Nandini2510@users.noreply.github.com>
2025-03-30 16:02:21 -0700

    Time: 0 ms (100%), Space: 17.9 MB (92.4%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -8,12 +8,21 @@ class Solution:
                 return
             if open > close and open != n:
                 dfs(open + 1, close, n, s + "(")
-                dfs(open , close + 1, n, s + ")")
+                dfs(open, close + 1, n, s + ")")
             if open == n:
                 dfs(open, close + 1, n, s + ")")
             if open == close:
                 dfs(open + 1, close, n, s + "(")
-            
+
+        
         dfs(0,0,n,"")
         return ans
+
+'''
+open before close
+1. if count open == close = terminate condition
+2. count of open > count of close = we can still create combinations
+3. open == n and close != n -> 
+'''
+
 
