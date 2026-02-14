246ae2ef2572c731c61853995bed88884b37006c
Nandani <52810541+Nandini2510@users.noreply.github.com>
2026-02-14 12:30:02 -0800

    Time: 14 ms (51.52%), Space: 22.2 MB (43.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,24 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mp = defaultdict(list)
-        ans = []
+        res = []
+        hashmap = defaultdict(list)
 
         for word in strs:
-            sorted_word = "".join(sorted(word))
-            if sorted_word in mp:
-                mp[sorted_word].append(word)
-            else:
-                mp[sorted_word].append(word)
+            sortedW = "".join(sorted(word))
+            hashmap[sortedW].append(word)
+        for k, v in hashmap.items():
+            res.append(v)
         
-        for val in mp.values():
-            ans.append(val)
-        return ans
+        return res
 
-
-
-'''
-1. Create a map of list, Iterate each w through list
-2. sort the w, check if sort w matches any key in map
-3. if it matches, we append the original w to that list of key value
-4. It it doesn't matches we will create a new pair (key, value)
-'''
+        
